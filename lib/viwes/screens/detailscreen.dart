ad7e7ec1e998025a36d90b8ff2ae251105615967
RAHILANDANI <randani143@rku.ac.in>
2024-01-01 11:13:28 +0530

    new

--- lib/viwes/screens/detailscreen.dart
+++ lib/viwes/screens/detailscreen.dart
@@ -3,6 +3,8 @@ import 'package:animator_galaxy_planet/provider/jsonprovider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../provider/theme_provider.dart';
+
 class DetailScreen extends StatefulWidget {
   int index;
   DetailScreen({super.key, required this.index});
@@ -34,59 +36,86 @@ class _DetailScreenState extends State<DetailScreen>
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            RotationTransition(
-              turns: animationController,
-              child: Container(
-                height: 400,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      provider.res['planetjson'][widget.index]['image'],
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: InkWell(
+                onTap: () {
+                  Provider.of<ThemeProvider>(context, listen: false)
+                      .ChangeTheme();
+                },
+                child: (Provider.of<ThemeProvider>(context).themeModel.isdark ==
+                        false)
+                    ? Icon(
+                        Icons.light_mode,
+                        color: Colors.white,
+                        size: 30,
+                      )
+                    : Icon(
+                        Icons.dark_mode,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RotationTransition(
+                  turns: animationController,
+                  child: Container(
+                    alignment: Alignment.topRight,
+                    height: 400,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          provider.res['planetjson'][widget.index]['image'],
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            Center(
-              child: Text(
-                "${provider.res['planetjson'][widget.index]['name']}",
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Description :-",
-                  style: TextStyle(fontSize: 22),
+                Center(
+                  child: Text(
+                    "${provider.res['planetjson'][widget.index]['name']}",
+                    style: TextStyle(fontSize: 30),
+                  ),
                 ),
-                ElevatedButton(
-                    onPressed: () {
-                      animateprovider.ChangeAnimation();
-                    },
-                    child: (animateprovider.animateModel.isanimated == false)
-                        ? Text("Click to get Description")
-                        : Text("Hide Description"))
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Description :-",
+                      style: TextStyle(fontSize: 22),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          animateprovider.ChangeAnimation();
+                        },
+                        child:
+                            (animateprovider.animateModel.isanimated == false)
+                                ? Text("Click to get Description")
+                                : Text("Hide Description"))
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                AnimatedContainer(
+                    curve: Curves.decelerate,
+                    duration: Duration(seconds: 4),
+                    height: (animateprovider.animateModel.isanimated == false)
+                        ? 0
+                        : 200,
+                    width: (animateprovider.animateModel.isanimated == false)
+                        ? 0
+                        : 300,
+                    child: Text(
+                        "${provider.res['planetjson'][widget.index]['description']}"))
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            AnimatedContainer(
-                curve: Curves.decelerate,
-                duration: Duration(seconds: 4),
-                height: (animateprovider.animateModel.isanimated == false)
-                    ? 0
-                    : 200,
-                width: (animateprovider.animateModel.isanimated == false)
-                    ? 0
-                    : 300,
-                child: Text(
-                    "${provider.res['planetjson'][widget.index]['description']}"))
           ],
         ),
       ),
